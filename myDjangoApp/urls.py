ec3140a1ce3edfa677b99b1e07f475219349619a
thor <tayyabmughal676@gmail.com>
2022-09-21 17:16:03 +0500

    CURD

--- myDjangoApp/urls.py
+++ myDjangoApp/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from listings.views import listing_list, listing_create, listing_retrieve
+from listings.views import (
+    listing_list, 
+    listing_create, 
+    listing_retrieve,
+    listing_update,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listing_list),
     path('listings/<pk>/', listing_retrieve),
+    path('listings/<pk>/edit/', listing_update),
     path("add-listing/", listing_create),
 ]
