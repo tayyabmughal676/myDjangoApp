002a2cd51c341edebfe105e524d377c1d03de022
thor <tayyabmughal676@gmail.com>
2022-09-21 15:50:10 +0500

    Init

---
+++ listings/models.py
@@ -0,0 +1,14 @@
+from ctypes import addressof
+from email.mime import image
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=100)
+    # image
